868916aaae1666ed4961449520714378894325a9
Wesclley <wesclley.rodrigues@gmail.com>
2022-09-26 22:07:08 -0300

    calculadora

---
+++ scripts.js
@@ -0,0 +1,59 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear()
+    }
+
+    chooseOperation(operation){
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    appendNumber(number){
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    updateDisplay(){
+        this.previousOperandTextElement.innerText = this.previousOperand;
+        this.currentOperandTextElement.innerText = this.currentOperand;
+    }
+}
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement);
+
+for (const numberButton of numberButtons){
+    numberButton.addEventListener('click', () => {
+        calculator.appendNumber(numberButton.innerText);
+        calculator.updateDisplay();
+    })
+}
+
+for (const operationButton of operationButtons){
+    operationButton.addEventListener('click', () => {
+        calculator.chooseOperation(operationButton.innerText)
+        calculator.updateDisplay();
+    })
+}
+
+allClearButton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+}
+)
